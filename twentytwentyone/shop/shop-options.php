7c470381f62cb3d16cfafbd0df6c67770a15c1b2
klaus <klaus.toska20@gmail.com>
2022-10-05 00:49:08 +0200

    as

--- twentytwentyone/shop/shop-options.php
+++ twentytwentyone/shop/shop-options.php
@@ -1,23 +1,23 @@
 <?php
 
-	// Define path and URL to the ACF plugin.
-	define( 'MY_ACF_PATH', get_stylesheet_directory() . '/shop/inc/acf/' );
-	define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/shop/inc/acf/' );
+	// // Define path and URL to the ACF plugin.
+	// define( 'MY_ACF_PATH', get_stylesheet_directory() . '/shop/inc/acf/' );
+	// define( 'MY_ACF_URL', get_stylesheet_directory_uri() . '/shop/inc/acf/' );
 
-	// Include the ACF plugin.
-	include_once( MY_ACF_PATH . 'acf.php' );
+	// // Include the ACF plugin.
+	// include_once( MY_ACF_PATH . 'acf.php' );
 
-	// Customize the url setting to fix incorrect asset URLs.
-	add_filter('acf/settings/url', 'my_acf_settings_url');
-	function my_acf_settings_url( $url ) {
-		return MY_ACF_URL;
-	}
+	// // Customize the url setting to fix incorrect asset URLs.
+	// add_filter('acf/settings/url', 'my_acf_settings_url');
+	// function my_acf_settings_url( $url ) {
+	// 	return MY_ACF_URL;
+	// }
 
-	// Hide the ACF admin menu item.
-	add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
-	function my_acf_settings_show_admin( $show_admin ) {
-		return false;
-	}
+	// // Hide the ACF admin menu item.
+	// add_filter('acf/settings/show_admin', 'my_acf_settings_show_admin');
+	// function my_acf_settings_show_admin( $show_admin ) {
+	// 	return false;
+	// }
 
 	function load_ajax(){
 		wp_enqueue_script('ajax', get_stylesheet_directory_uri(). '/shop/js/ajax-fiter.js' , array('jquery'), NULL, true);
