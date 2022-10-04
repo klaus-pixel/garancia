5dccdd1c04ce626ba2ba0b559f902505bab124a7
klaus <klaus.toska20@gmail.com>
2022-10-04 11:54:59 +0200

    taxanomi

--- twentytwentyone/shop/shop-options.php
+++ twentytwentyone/shop/shop-options.php
@@ -385,3 +385,40 @@ function cptui_register_my_cpts() {
 }
 
 add_action( 'init', 'cptui_register_my_cpts' );
+
+function cptui_register_my_taxes_tipi() {
+
+	/**
+	 * Taxonomy: Tipi.
+	 */
+
+	$labels = [
+		"name" => esc_html__( "Tipi", "twentytwentyone" ),
+		"singular_name" => esc_html__( "Tipi", "twentytwentyone" ),
+	];
+
+	
+	$args = [
+		"label" => esc_html__( "Tipi", "twentytwentyone" ),
+		"labels" => $labels,
+		"public" => true,
+		"publicly_queryable" => true,
+		"hierarchical" => false,
+		"show_ui" => true,
+		"show_in_menu" => true,
+		"show_in_nav_menus" => true,
+		"query_var" => true,
+		"rewrite" => [ 'slug' => 'tipi', 'with_front' => true, ],
+		"show_admin_column" => false,
+		"show_in_rest" => true,
+		"show_tagcloud" => false,
+		"rest_base" => "tipi",
+		"rest_controller_class" => "WP_REST_Terms_Controller",
+		"rest_namespace" => "wp/v2",
+		"show_in_quick_edit" => false,
+		"sort" => false,
+		"show_in_graphql" => false,
+	];
+	register_taxonomy( "tipi", [ "produkte" ], $args );
+}
+add_action( 'init', 'cptui_register_my_taxes_tipi' );
